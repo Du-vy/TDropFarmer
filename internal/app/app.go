cb26a747e88c4b167eedf78c467a508dfbe92487
Duvy <200692241+Du-vy@users.noreply.github.com>
2026-06-26 02:35:14 -0300

    cleanup

--- internal/app/app.go
+++ internal/app/app.go
@@ -628,7 +628,7 @@ func (a *App) runMinuteWatched(ctx context.Context, eng *engine.Engine, gqlClien
 					)
 					continue
 				} else {
-					a.logger.Info("minute watched sent",
+					a.logger.Warn("minute watched sent",
 						slog.String("streamer", login),
 						slog.String("game", s.GameName),
 					)
